e560c46dddba4181896249e206fd16a7056097dd
Nick Wood <nickswift498@gmail.com>
2014-04-05 16:22:28 -0700

    removed settings button from login screen

--- java/com/yankteam/yank/app/LoginActivity.java
+++ java/com/yankteam/yank/app/LoginActivity.java
@@ -16,7 +16,7 @@ public class LoginActivity extends ActionBarActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.main, menu);
+        // getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
 
@@ -25,10 +25,10 @@ public class LoginActivity extends ActionBarActivity {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
+        /*int id = item.getItemId();
         if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
         return super.onOptionsItemSelected(item);
     }
 
