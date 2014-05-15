45cbb29b7d33ad5721f69df0716cdaca50f882a4
Nick Wood <nickswift498@gmail.com>
2014-05-14 19:32:56 -0700

    assorted cleanup

--- app/src/main/java/com/yankteam/yank/app/LobbyActivity.java
+++ app/src/main/java/com/yankteam/yank/app/LobbyActivity.java
@@ -1,7 +1,4 @@
 package com.yankteam.yank.app;
-
-
-import android.app.Activity;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -10,38 +7,28 @@ import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
-import android.view.LayoutInflater;
-import android.view.View;
-import android.view.ViewGroup;
-import android.widget.TextView;
 
-import com.yankteam.yank.app.Components.LobbyPagerAdapter;
-
-import java.util.Locale;
 import java.util.List;
 import java.util.Vector;
-import android.support.v4.app.FragmentActivity;
 
-/**
- * Created by nick on 4/15/2014.
- */
 public class LobbyActivity extends ActionBarActivity implements ActionBar.TabListener {
+    private SectionsPagerAdapter mSectionsPagerAdapter;
+    private ViewPager mViewPager;
 
-    SectionsPagerAdapter mSectionsPagerAdapter;
-    ViewPager mViewPager;
+    // Fragment manager
+    public static FragmentManager fragmentManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_lobby);
 
-        // Set up the action bar
-
+        // set up the action bar
         final ActionBar actionBar = getSupportActionBar();
         actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
 
+        // set up the view pager
         initializePager();
-
         mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener(){
             @Override
             public void onPageSelected(int position) {
@@ -55,34 +42,37 @@ public class LobbyActivity extends ActionBarActivity implements ActionBar.TabLis
                         .setText(mSectionsPagerAdapter.getPageTitle(i))
                         .setTabListener(this));
         }
+
+        // establish the fragment manager
+        fragmentManager = getSupportFragmentManager();
     }
 
     private void initializePager() {
-
+        // create a list to hold fragments
         List<Fragment> fragments = new Vector<Fragment>();
         fragments.add(Fragment.instantiate(this, MapFragment.class.getName()));
         fragments.add(Fragment.instantiate(this, SavedFragment.class.getName()));
         fragments.add(Fragment.instantiate(this, ListFragment.class.getName()));
+
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager(), fragments);
         mViewPager = (ViewPager) findViewById(R.id.lobby_pager);
         mViewPager.setAdapter(mSectionsPagerAdapter);
-        mViewPager.setOffscreenPageLimit(4);
-
+        mViewPager.setOffscreenPageLimit(3);
     }
+
 
     @Override
     public void onTabSelected(ActionBar.Tab tab, FragmentTransaction ft) {
         mViewPager.setCurrentItem(tab.getPosition());
     }
 
+    // stubs necessitated by implementing TabListener
     @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {
-    }
-
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction ft) {}
     @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {
-    }
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction ft) {}
 
+    // the pager adapter
     public class SectionsPagerAdapter extends FragmentPagerAdapter {
 
         private List<Fragment> fragments;
@@ -94,20 +84,16 @@ public class LobbyActivity extends ActionBarActivity implements ActionBar.TabLis
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a fragment in the fragment container
-            return this.fragments.get(position);
+            return fragments.get(position);
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
             return this.fragments.size();
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
-            Locale l = Locale.getDefault();
             switch (position) {
                 case 0:
                     return "MAP VIEW";
@@ -119,35 +105,4 @@ public class LobbyActivity extends ActionBarActivity implements ActionBar.TabLis
             return null;
         }
     }
-
-   /* public static class PlaceholderFragment extends Fragment {
-        *//**
-         * The fragment argument representing the section number for this
-         * fragment.
-         *//*
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        *//**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         *//*
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_lobby_map, container, false);
-            return rootView;
-        }
-    }*/
-
 }
--- app/src/main/java/com/yankteam/yank/app/MapFragment.java
+++ app/src/main/java/com/yankteam/yank/app/MapFragment.java
@@ -1,91 +1,54 @@
 package com.yankteam.yank.app;
 
-import android.app.Dialog;
-import android.location.Criteria;
-import android.location.*;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.SupportMapFragment;
-import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
-import com.google.android.gms.maps.model.MarkerOptions;
 
-
-/**
- * Created by root on 5/12/14.
- */
 public class MapFragment extends Fragment {
 
+    private SupportMapFragment mMapFragment;
+    private GoogleMap mMap;
 
-    private GoogleMap map;
-    private Location location;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle saveInstanceState) {
-        if (container == null)
-            return null;
-        /*int status = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-        if(status!=ConnectionResult.SUCCESS){ // Google Play Services are not available
-
-            int requestCode = 10;
-            Dialog dialog = GooglePlayServicesUtil.getErrorDialog(status, this, requestCode);
-            dialog.show();
-
-        }
-*/
-        View v = inflater.inflate(R.layout.fragment_lobby_map, container, false);
-
-        return v;
-
+        return inflater.inflate(R.layout.fragment_lobby_map, container, false);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
-        map = ((SupportMapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+        FragmentManager fm = getChildFragmentManager();
+        mMapFragment = (SupportMapFragment) fm.findFragmentById(R.id.lobby_map);
 
-      /*  LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        if (mMapFragment == null) {
+            mMapFragment = SupportMapFragment.newInstance();
+            fm.beginTransaction()
+                    .replace(R.id.lobby_map, mMapFragment)
+                    .commit();
+        }
+    }
 
-        // Creating a criteria object to retrieve provider
-        Criteria criteria = new Criteria();
+    @Override
+    public void onResume() {
+        super.onResume();
+        if (mMap == null) {
+            mMap = mMapFragment.getMap();
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(true);
+            mMap.getUiSettings().setRotateGesturesEnabled(true);
+            mMap.getUiSettings().setCompassEnabled(true);
 
-        // Getting the name of the best provider
-        String provider = locationManager.getBestProvider(criteria, true);
-
-        // Getting Current Location
-        location = locationManager.getLastKnownLocation(provider);
-        LocationListener locationListener = new LocationListener() {
-            @Override
-            public void onLocationChanged(Location location) {
-                drawMarker(location);
-            }
-        };
-
-        locationManager.requestLocationUpdates(provider, 20000, 0, locationListener);*/
-
-        map.setMyLocationEnabled(true);
-        map.getUiSettings().setMyLocationButtonEnabled(true);
-        map.getUiSettings().setRotateGesturesEnabled(true);
-
-        map.setOnMyLocationChangeListener(new GoogleMap.OnMyLocationChangeListener() {
-
-            @Override
-            public void onMyLocationChange(Location location) {
-                map.addMarker(new MarkerOptions().position(new LatLng(location.getLatitude(),
-                        location.getLongitude())).title("You're Here"));
-                CameraPosition cameraPosition = new CameraPosition.Builder().target(
-                        new LatLng(location.getLatitude(), location.getLongitude())).zoom(12).build();
-
-                map.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
-            }
-        });
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                    new LatLng(36.989750, -122.059221), 14));
+        }
     }
 }
