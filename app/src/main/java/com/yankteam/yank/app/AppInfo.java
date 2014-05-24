edd95f166b1b51001b9bafc66a2166bdcfa32611
Jason Heron <JasonAHeron@Gmail.com>
2014-05-24 14:03:18 -0700

    login working in app

--- app/src/main/java/com/yankteam/yank/app/AppInfo.java
+++ app/src/main/java/com/yankteam/yank/app/AppInfo.java
@@ -2,7 +2,7 @@ package com.yankteam.yank.app;
 
 /*
  * Singleton appinfo class
- *
  */
 public class AppInfo {
+    public final static String api_url = "http://jheron.io";
 }
--- app/src/main/java/com/yankteam/yank/app/LoginActivity.java
+++ app/src/main/java/com/yankteam/yank/app/LoginActivity.java
@@ -6,35 +6,66 @@ import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.NameValuePair;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
 import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.message.BasicNameValuePair;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 public class LoginActivity extends ActionBarActivity {
 
+    public static final String LOG_TAG = "LoginActivity";
+
     Button mLoginButton;
     Button mSignupButton;
+
+    EditText mEtxtUsername;
+    EditText mEtxtPassword;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        // Get references to layout components
         mLoginButton  = (Button) findViewById(R.id.btn_login);
         mSignupButton = (Button) findViewById(R.id.btn_signup);
+
+        mEtxtUsername = (EditText) findViewById(R.id.etxt_username);
+        mEtxtPassword = (EditText) findViewById(R.id.etxt_password);
 
         // For now, we're just going to switch automatically
         mLoginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                gotoLobby();
+                // initiate an asynctask
+                new LoginTask().execute(
+                        mEtxtUsername.getText().toString(),
+                        mEtxtPassword.getText().toString() );
             }
         });
     }
@@ -44,9 +75,8 @@ public class LoginActivity extends ActionBarActivity {
     @Override
     public void onResume() {
         super.onResume();
-
         SharedPreferences sPrefs = this.getPreferences(Context.MODE_PRIVATE);
-        String apik = sPrefs.getString("YANK_APIK", null);
+        String apik              = sPrefs.getString("YANK_APIK", null);
 
         // if there's an APIK, try it
         if (apik != null) {
@@ -66,6 +96,72 @@ public class LoginActivity extends ActionBarActivity {
             DefaultHttpClient httpClient = new DefaultHttpClient();
             HttpPost postReq = new HttpPost();
             return null;
+        }
+    }
+
+    private class LoginTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... args) {
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost postReq = new HttpPost(getString(R.string.jheron_api) + "/auth/login/");
+            HttpResponse response = null;
+
+            // Set up a JSON request body
+            JSONObject json = new JSONObject();
+            try {
+                // Prepare post data
+                json.put("username", args[0] );
+                json.put("password", args[1] );
+
+                // Assemble request
+                StringEntity entity = new StringEntity(json.toString());
+                entity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                postReq.setEntity(entity);
+
+                // Send request
+                response = httpClient.execute(postReq);
+
+                // Pull apart the request with an input stream
+                if (response != null){
+                    // set up Java's bullshit
+                    InputStream in = response.getEntity().getContent();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder builder = new StringBuilder();
+
+                    // Let Java do some bullshit
+                    String line = "";
+                    while ((line = reader.readLine()) != null) {
+                        builder.append(line);
+                    }
+
+                    // return Java's bullshit
+                    return builder.toString();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                // TODO: JSON exception
+                // basically, quit
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                // TODO: String entity exception
+            } catch (ClientProtocolException e) {
+                // TODO: incorrect protocol exception
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO: I/O exception
+                e.printStackTrace();
+            }
+
+            // Return null if failure occurs
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+
+            // TODO: UI THREAD STUFF
+            // Log.d(LoginActivity.LOG_TAG, s);
         }
     }
 }
