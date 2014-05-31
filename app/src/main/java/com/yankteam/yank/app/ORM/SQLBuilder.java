5df076cac315142f4c4a125c64e067a13fad6eaf
Nick Wood <nickswift498@gmail.com>
2014-05-31 14:34:33 -0700

    adding CREATE/DELETE

---
+++ app/src/main/java/com/yankteam/yank/app/ORM/SQLBuilder.java
@@ -0,0 +1,43 @@
+package com.yankteam.yank.app.ORM;
+
+/*
+ * SQLBuilder
+ * Builds SQL automatically, because seriously... fuck multiline strings.
+ */
+public class SQLBuilder {
+
+    // Create a table
+    public String createTable (String name, SQLColumn[] cols) {
+
+        StringBuilder sql = new StringBuilder();
+        sql.append("CREATE TABLE ");
+        sql.append(name);
+        sql.append("(");
+
+        // We always add a primary key
+        SQLColumn pk = new SQLColumn("id", SQLColumn.TYPE_INTEGER,
+                "primary key autoincrement");
+        sql.append(pk.getColumnDecl());
+        sql.append(",");
+
+        // Iterate over columns
+        for (int i=0; i<cols.length; i++){
+            // get column declaration
+            sql.append(cols[i].getColumnDecl());
+
+            // append the correct number of commas
+            if (i < cols.length-1) {
+                sql.append(",");
+            }
+        }
+
+        // finish the statement
+        sql.append(");");
+        return sql.toString();
+    }
+
+    // Drop a table
+    public String dropTable(String name){
+        return "DROP TABLE IF EXISTS" + name;
+    }
+}
