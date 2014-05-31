506d83c89ed5f7351d677dfd99e8c5561cde3499
Jason Heron <JasonAHeron@Gmail.com>
2014-05-31 13:46:45 -0700

    working lat,lng from real life

--- app/src/main/java/com/yankteam/yank/app/lobbyfragments/MapFragment.java
+++ app/src/main/java/com/yankteam/yank/app/lobbyfragments/MapFragment.java
@@ -1,9 +1,13 @@
 package com.yankteam.yank.app.lobbyfragments;
 
+import android.content.Context;
 import android.location.Location;
+import android.location.LocationManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -21,10 +25,32 @@ import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.CameraPosition;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.yankteam.yank.app.AppInfo;
 import com.yankteam.yank.app.R;
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 public class MapFragment extends Fragment implements GooglePlayServicesClient.ConnectionCallbacks,
         GooglePlayServicesClient.OnConnectionFailedListener, LocationListener {
+
+    public static final String LOG_TAG = "MapFragment";
+    AppInfo appInfo = AppInfo.getInstance();
+
 
     private SupportMapFragment mMapFragment;
     private GoogleMap mMap;
@@ -33,6 +59,11 @@ public class MapFragment extends Fragment implements GooglePlayServicesClient.Co
     private LocationClient mLocationClient;
     private LocationRequest mLocationRequest;
     private Location mCurrentLocation;
+    LocationManager lm;
+    Location location;
+    double latitude = 0.0;
+    double longitude = 0.0;
+
 
     // Location data -- needed to make use of location service
     private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
@@ -51,7 +82,30 @@ public class MapFragment extends Fragment implements GooglePlayServicesClient.Co
             Toast.makeText(getActivity(), GooglePlayServicesUtil.getErrorString(playStatus),
                     Toast.LENGTH_SHORT).show();
         }
-        return inflater.inflate(R.layout.fragment_lobby_map, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_lobby_map, container, false);
+        Context context = view.getContext();
+
+        lm = (LocationManager)context.getSystemService(context.LOCATION_SERVICE);
+        location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location != null) {
+            if(LatLngChange(location.getLatitude(), location.getLongitude())){
+            longitude = location.getLongitude();
+            latitude = location.getLatitude();
+            Log.d(MapFragment.LOG_TAG, "found lon: " + longitude + " found lat: " + latitude);
+            new MapPopulateTask().execute(5000.0 , latitude, longitude);
+            }
+        } else {
+            longitude = 0.0;
+            latitude = 0.0;
+        }
+        return view;
+    }
+
+    public boolean LatLngChange(double newLat, double newLng){
+    //if rounded public lattitude != rounded newLat return true
+    //if rounded public longitude != rounded newLng return true
+        return true;
     }
 
     @Override
@@ -67,6 +121,7 @@ public class MapFragment extends Fragment implements GooglePlayServicesClient.Co
                     .replace(R.id.lobby_map, mMapFragment)
                     .commit();
         }
+
     }
 
     @Override
@@ -115,8 +170,22 @@ public class MapFragment extends Fragment implements GooglePlayServicesClient.Co
 
     @Override
     public void onLocationChanged(Location location) {
-        mLocationClient.removeLocationUpdates(this);
+
+        location = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location != null) {
+            if(LatLngChange(location.getLatitude(), location.getLongitude())){
+                longitude = location.getLongitude();
+                latitude = location.getLatitude();
+                Log.d(MapFragment.LOG_TAG, "found lon: " + longitude + " found lat: " + latitude);
+                new MapPopulateTask().execute(5000.0 , latitude, longitude);
+            }
+        } else {
+            longitude = 0.0;
+            latitude = 0.0;
+        }
+
         recenterCamera();
+
     }
 
     @Override
@@ -134,4 +203,109 @@ public class MapFragment extends Fragment implements GooglePlayServicesClient.Co
                         mCurrentLocation.getLongitude())).zoom(12).build();
         mMap.moveCamera(CameraUpdateFactory.newCameraPosition(cPos));
     }
+
+
+    //need radius, lat, lon
+    private class MapPopulateTask extends AsyncTask<Double, Void, String> {
+        @Override
+        protected String doInBackground(Double... args) {
+            DefaultHttpClient httpClient = new DefaultHttpClient();
+            HttpPost postReq = new HttpPost(getString(R.string.jheron_api) + "entity/radius/");
+            HttpResponse response;
+
+            // Set up a JSON request body
+            JSONObject json = new JSONObject();
+            try {
+                // Prepare post data
+                json.put("radius", args[0] );
+                json.put("lat", args[1] );
+                json.put("lng", args[2] );
+
+                // Assemble request
+                StringEntity entity = new StringEntity(json.toString());
+                entity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+                postReq.setEntity(entity);
+
+                Log.d(MapFragment.LOG_TAG, "sending: " + entity.toString());
+
+                // Send request
+                response = httpClient.execute(postReq);
+
+                // Pull apart the request with an input stream
+                if (response != null){
+                    // set up Java's bullshit
+                    InputStream in = response.getEntity().getContent();
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+                    StringBuilder builder = new StringBuilder();
+
+                    // Let Java do some bullshit
+                    String line;
+                    while ((line = reader.readLine()) != null) {
+                        builder.append(line);
+                    }
+
+                    // return Java's bullshit
+                    return builder.toString();
+                }
+            } catch (JSONException e) {
+                e.printStackTrace();
+                // TODO: JSON exception
+                // basically, quit
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+                // TODO: String entity exception
+            } catch (ClientProtocolException e) {
+                // TODO: incorrect protocol exception
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO: I/O exception
+                e.printStackTrace();
+            }
+
+            // Return null if failure occurs
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+            try {
+                Log.d(MapFragment.LOG_TAG, "map json: " + s);
+                JSONObject mainObject = new JSONObject(s);
+                Boolean success_response = mainObject.getBoolean("success");
+                String msg_response = mainObject.getString("msg");
+                JSONArray data_response = mainObject.getJSONArray("data");
+
+                if(success_response){
+                    //ORM STUFF
+                    for(int n = 0; n < data_response.length(); n++){
+                        JSONObject object = data_response.getJSONObject(n);
+                        Log.d(MapFragment.LOG_TAG, "Found: " + object.toString());
+                    }
+                }else {
+                    //shit failed
+                    Log.d(MapFragment.LOG_TAG, "map failed: likely user");
+                    Log.d(MapFragment.LOG_TAG, msg_response);
+
+                    //toast user, bad login
+                }
+
+
+
+            } catch (JSONException e) {
+                //ask user to retry login
+                e.printStackTrace();
+                Log.d(MapFragment.LOG_TAG, "map failed: likely at server");
+
+            }
+
+            // TODO: UI THREAD STUFF
+            // Log.d(LoginActivity.LOG_TAG, s);
+        }
+    }
+
+
+
+
+
 }
